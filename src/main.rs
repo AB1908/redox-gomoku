d354895a4c4f4e0ead5f80ef9004fe3877f16c3b
AB1908 <abhishekbhattacherjee@live.com>
2018-12-01 20:11:23 +0530

    Refactor code to use functions for grid gen
    - Grid states have now been accounted for.

--- src/main.rs
+++ src/main.rs
@@ -1,147 +1,187 @@
 extern crate termion;
 
 // use std::fmt::Write;
-use std::env;
-use std::io;
-use termion::raw::IntoRawMode;
+use std::io::Read;
+// use std::env;
+// use std::io;
+use std::io::{stdin, stdout, Write};
+use std::string::String;
 use termion::event::Key;
 use termion::input::TermRead;
-use std::io::{Write, stdout, stdin};
+use termion::raw::IntoRawMode;
 use termion::*;
-use std::string::String;
 
-const top_left: &'static str = "┌";
-const top: &'static str = "┬";
-const top_right: &'static str = "┐";
-const vert: &'static str = "│";
-const left: &'static str = "├";
-const right: &'static str = "┤";
-const inside: &'static str = "┼";
-const bot_left: &'static str = "└";
-const bot: &'static str = "┴";
-const bot_right: &'static str = "┘";
-const horz: &'static str = "---";
-const cell: &'static str = "   ";
-const player1: &'static str = " X ";
-const player2: &'static str = " O ";
-const player_marker: &'static str = " # ";
+const TOP_LEFT: &'static str = "┌";
+const TOP: &'static str = "┬";
+const TOP_RIGHT: &'static str = "┐";
+const VERT: &'static str = "│";
+const LEFT: &'static str = "├";
+const RIGHT: &'static str = "┤";
+const INSIDE: &'static str = "┼";
+const BOT_LEFT: &'static str = "└";
+const BOT: &'static str = "┴";
+const BOT_RIGHT: &'static str = "┘";
+const HORZ: &'static str = "---";
+const CELL: &'static str = "   ";
+const PLAYER1: &'static str = " X ";
+const PLAYER2: &'static str = " O ";
+//const PLAYER_MARKER: &'static str = " # ";
 
-fn print_grid(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
-    for i in 0..31 {
-        create_row(stdout, i);
-    }
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+enum CellStatus {
+    Player1Marked,
+    Player2Marked,
+    NotMarked,
 }
 
-fn create_row(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, row_num: i32) {
-    let mut row;
-    if row_num == 0 {
-        row = String::from(top_left);
-        row.push_str(horz);
-    } else if row_num == 30 {
-        row = String::from(bot_left);
-        row.push_str(horz);
-    } else if row_num % 2 == 0 {
-        row = String::from(left);
-        row.push_str(horz);
-    } else {
-        row = String::from(vert);
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+struct Cell {
+    player_here: bool,
+    status: CellStatus,
+    x: i32,
+    y: i32,
+}
+
+struct Game<R, W: Write> {
+    status: bool,
+    x: i32,
+    y: i32,
+    width: i32,
+    grid: Box<[Cell]>,
+    stdout: W,
+    stdin: R,
+}
+
+/// Initialize the game.
+fn init<W: Write, R: Read>(mut stdout: W, stdin: R, w: i32, h: i32) {
+    write!(stdout, "{}", clear::All).unwrap();
+
+    // Set the initial game state.
+    let mut game = Game {
+        x: 0,
+        y: 0,
+        status: false,
+        width: w,
+        grid: vec![Cell {
+            player_here: false,
+            x: 0,
+            y: 0,
+            status: CellStatus::NotMarked,
+        }; w as usize * h as usize].into_boxed_slice(),
+        stdin: stdin.keys(),
+        stdout: stdout,
+    };
+
+    // Reset that game.
+    // game.reset();
+    // game.draw_tile_row(2);
+    game.draw_grid();
+}
+
+impl<R: Iterator<Item=Result<Key, std::io::Error>>, W: Write> Game<R, W> {
+    
+    fn pos(&self, x: i32, y: i32) -> usize {
+        y as usize * self.width as usize + x as usize
+
     }
 
-    for i in 0..14 {
-        if row_num == 0 {
-            row.push_str(top);
-            row.push_str(horz);
-        } else if row_num == 30 {
-            row.push_str(bot);
-            row.push_str(horz);
-        } else if row_num % 2 == 1 {
-            row.push_str(cell);
-            row.push_str(vert);
-        } else if row_num % 2 == 0 {
-            row.push_str(inside);
-            row.push_str(horz);
+    fn read_cell(&mut self, c: usize) -> CellStatus {
+        self.grid[c].status
+    }
+
+    fn get(&mut self, x: i32, y: i32) -> Cell {
+        let pos = self.pos(x, y);
+
+        self.read_cell(pos);
+        self.grid[pos]
+    }
+/// Get a mutable reference to the cell at (x, y).
+    fn get_mut(&mut self, x: i32, y: i32) -> &mut Cell {
+
+        let pos = self.pos(x, y);
+        self.read_cell(pos);
+        &mut self.grid[pos]
+    }
+
+    fn draw_tile_row(&mut self, y: i32) {
+        // let cell = self.get(x, y);
+        let mut row;
+        row = String::from("");
+        for i in 0..15 {
+        row.push_str(VERT);
+        let cell = self.get(i, y);
+        match cell.status {
+            CellStatus::Player1Marked => row.push_str(PLAYER1),
+            CellStatus::Player2Marked => row.push_str(PLAYER2),
+            CellStatus::NotMarked => row.push_str(CELL),
         }
     }
-    // row.push_str(horz);
-    if row_num == 0 {
-        row.push_str(top_right);
-    } else if row_num == 30 {
-        row.push_str(bot_right);
-    } else if row_num % 2 == 1 {
-        row.push_str(cell);
-        row.push_str(vert);
-    } else {
-        row.push_str(right);
+    row.push_str(VERT);
+    write!(self.stdout, "{}\n\r", row).unwrap();
     }
-    stdout.write(row.as_bytes()).unwrap();
-    write!(stdout, "\n\r");
-    // writeln!(stdout, "{}", row);
-    // println!("{}", row);
-    // write!(stdout, "{}", row).unwrap();
+
+    fn draw_horizontal_wall(&mut self)
+
+    {
+        let mut row;
+        row = String::from(LEFT);
+        for _i in 0..14 {
+            row.push_str(HORZ);
+            row.push_str(INSIDE);
+        }
+        row.push_str(HORZ);
+        row.push_str(RIGHT);
+        write!(self.stdout, "{}\n\r", row).unwrap();
+    }
+
+    fn draw_top_wall(&mut self)
+    {
+        let mut row;
+        row = String::from(TOP_LEFT);
+        for _i in 0..14 {
+            row.push_str(HORZ);
+            row.push_str(TOP);
+        }
+        row.push_str(HORZ);
+        row.push_str(TOP_RIGHT);
+        write!(self.stdout, "{}\n\r", row).unwrap();
+    }
+
+    fn draw_bottom_wall(&mut self)
+    {
+        let mut row;
+        row = String::from(BOT_LEFT);
+        for _i in 0..14 {
+            row.push_str(HORZ);
+            row.push_str(BOT);
+        }
+        row.push_str(HORZ);
+        row.push_str(BOT_RIGHT);
+        write!(self.stdout, "{}", row).unwrap();
+    }
+
+    fn draw_grid(&mut self)
+    {
+        self.draw_top_wall();
+        for i in 0..15 {
+            self.draw_tile_row(i);
+            if i < 14 {
+                self.draw_horizontal_wall();
+            }
+        }
+        self.draw_bottom_wall();
+    }
 }
 
 fn main() {
-    // println!("{}", clear::All);
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
-    // write!(stdout, "Gomoku for Redox");
-    //  write!(stdout, "{}{}q to exit. Type stuff, use alt, and so on.{}",
-    //       // Clear the screen.
-    //       termion::clear::All,
-    //       // Goto (1,1).
-    //       termion::cursor::Goto(1, 1),
-    //       // Hide the cursor.
-    //       termion::cursor::Hide).unwrap();
-    //// Flush stdout (i.e. make the output appear).
-    //stdout.flush().unwrap();
-    //for c in stdin.keys() {
-    //    // Clear the current line.
-    //    write!(stdout, "{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine).unwrap();
-//
-    //    // Print the key we type...
-    //    match c.unwrap() {
-    //        // Exit.
-    //        Key::Char('q') => break,
-    //        Key::Char(c)   => println!("{}", c),
-    //        Key::Alt(c)    => println!("Alt-{}", c),
-    //        Key::Ctrl(c)   => println!("Ctrl-{}", c),
-    //        Key::Left      => println!("<left>"),
-    //        Key::Right     => println!("<right>"),
-    //        Key::Up        => println!("<up>"),
-    //        Key::Down      => println!("<down>"),
-    //        _              => println!("Other"),
-    //    }
-//
-    //    // Flush again.
-    //    stdout.flush().unwrap();
-    //}
-    //write!(stdout, "{}", termion::cursor::Show).unwrap();
-    
-    println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
-    // println!("Hello there!");
+    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
     let termsize = termion::terminal_size().ok().unwrap();
     let termwidth = termsize.0;
-    let mut game_grid: [[bool; 15]; 15];
-    // let termheight = termsize.1;
-    let termheight = 15;
+    // let termheight = ;
     let mut input = String::new();
-    print_grid(&mut stdout);
-    // while input != "q" {
-    // println!("{}{}",termion::clear::All, termion::cursor::Goto(1,1));
-    // for i in 0..termheight {
-    // if i == 0 {
-    // println!("{}{}{}{}", top_left, cell, top, top_right);
-    // }
-    // println!("├---┼---┼---┼---┼---┼---┼---┼---┼---┼---┼---┼---┼---┼---┼---┤");
-    // println!("│   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │");
-    // if i == termheight - 1 {
-    // println!("{}{}{}{}", bot_left, cell, bot, bot_right);
-    // }
-    // }
-    // io::stdin().read_line(&mut input).expect("error: unable to read user input");
-    // let b = io::stdin().unwrap().unwrap();
-    // if input == "d" {
-    // termion::raw::IntoRawMode;
-    // }
-    // }
+    // print_grid(&mut stdout);
+    write!(stdout, "{}{}{}", clear::All, style::Reset, cursor::Goto(1, 1)).unwrap();
+    init(stdout, stdin, 15, 15);
 }
